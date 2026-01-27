2fcf2f28852877ba6e04e8a4d3f56d605975e7fe
Mi <ngoc.hong0001@stud.hkr.se>
2026-01-28 00:43:38 +0100

    1 server & 1 client communication done!

--- tcp_client.py
+++ tcp_client.py
@@ -2,9 +2,20 @@ import socket
 
 #create a client side IPV4 socket (AF_INET) and TCP (SOCK_STREAM)
 #no close() needed when using with statement.
+
+HOST_IP = socket.gethostbyname(socket.gethostname())
+PORT = 65432
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.connect((socket.gethostbyname(socket.gethostname()), 65432)) #connect the socket to a serber located at a given IP & PORT
+  s.connect((HOST_IP, PORT)) #connect the socket to a server located at a given IP & PORT
+
+  print(f"Connected to server at {HOST_IP}:{PORT}")
   
-  #Recieve a message fr. the server...must specify the max nbr of bytes to recv.
-  message = s.recv(1024) #1024 bytes max
-  print(message) 
+  #write message to server
+  message = input("-> ") #take input fr. user
+
+  while message.lower().strip() != 'bye':
+    s.send(message.encode()) #send data to the server (must be encoded to bytes)
+    data = s.recv(1024).decode() #recv. resp. fr. server & decode it to string
+    print("Received from server: ",data) 
+    message = input("-> ") #take input fr. client again
